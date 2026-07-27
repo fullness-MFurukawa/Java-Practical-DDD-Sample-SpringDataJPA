2f4fe45039d1dd91fda567f37f03b773145a201c
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-07-27 09:22:20 +0900

    first commit

---
+++ src/main/java/jp/co/fullness/ddd/sample/PracticalSampleSpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package jp.co.fullness.ddd.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalSampleSpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalSampleSpringdatajpaApplication.class, args);
+	}
+
+}
